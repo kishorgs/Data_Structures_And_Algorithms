1c6d2b9505d50f1168b22e3d78441e41a5ee19f4
kishor g s <78294982+kishorgs@users.noreply.github.com>
2024-12-12 00:10:14 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -2,7 +2,7 @@ class patterns {
     public static void pattern1() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                System.out.print("*");
+                System.out.print("*");      
             }
             System.out.println();
         }
