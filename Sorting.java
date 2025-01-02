6892d9f86574d680508b4faecca659798333db21
kishor g s <78294982+kishorgs@users.noreply.github.com>
2025-01-03 01:09:52 +0530

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,26 +1,6 @@
 class Sorting {
-    // Swap method
-    void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
 
-    // Insertion Sort
-    // This algorithm divides the array into a sorted and unsorted part. It iteratively takes elements from the unsorted part and inserts them into the correct position in the sorted part.
-    void insertion_sort(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
-            int j = i;
-            while (j > 0 && arr[j - 1] > arr[j]) {
-                swap(arr, j, j - 1);
-                j--;
-            }
-        }
-        System.out.println("Array after Insertion Sort:");
-        printArray(arr);
-    }
-
-    // Bubble Sort
+     // Bubble Sort
     // This algorithm repeatedly compares adjacent elements and swaps them if they are in the wrong order. It "bubbles" the largest element to the end of the array in each pass.
     void bubble_sort(int[] arr, int n) {
         for (int i = 0; i < n - 1; i++) {
@@ -50,12 +30,33 @@ class Sorting {
         printArray(arr);
     }
 
-    // Utility method to print the array
+    // Insertion Sort
+    // This algorithm divides the array into a sorted and unsorted part. It iteratively takes elements from the unsorted part and inserts them into the correct position in the sorted part.
+    void insertion_sort(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                swap(arr, j, j - 1);
+                j--;
+            }
+        }
+        System.out.println("Array after Insertion Sort:");
+        printArray(arr);
+    } 
+
+    // Method to print the array
     void printArray(int[] arr) {
         for (int num : arr) {
             System.out.print(num + " ");
         }
         System.out.println();
+    }
+
+    // Swap method
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void main(String[] args) {
